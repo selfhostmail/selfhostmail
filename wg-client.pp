7ce35099b9c9f0f1ac165d3825e2cb745451f934
colonelpanik <kalielaeolian@gmail.com>
2022-01-09 08:35:26 -0800

    Make wg cliet match instructions

--- wg-client.pp
+++ wg-client.pp
@@ -3,7 +3,7 @@ file { '/etc/wireguard':
   mode   => '0600'
 }
 exec {'wireguard client enable':
-  command => "/usr/bin/cp /root/`hostname -s`-wg0.conf /etc/wireguard/wg0.conf; sudo chmod 0700 /etc/wireguard/wg0.conf; /usr/bin/systemctl enable wg-quick@wg0; /usr/bin/systemctl start wg-quick@wg0; \
+  command => "/usr/bin/cp /root/wg0.conf /etc/wireguard/wg0.conf; sudo chmod 0700 /etc/wireguard/wg0.conf; /usr/bin/systemctl enable wg-quick@wg0; /usr/bin/systemctl start wg-quick@wg0; \
   /usr/bin/firewall-cmd --permanent --zone=trusted --add-interface=wg0; \
   /usr/bin/firewall-cmd --reload",
   unless  => "/usr/bin/systemctl is-enabled wg-quick@wg0"
